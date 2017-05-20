61d6e6b264ec3c4f33bc43c86d00481a609e3767
shady <huangdongyang@honraytech.com>
2017-05-20 15:22:18 +0800

    Config the static and media direcotry

--- Config/settings.py
+++ Config/settings.py
@@ -128,3 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'collectstatic', 'static_root')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'collectstatic', 'media_root')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
